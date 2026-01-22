878c8df71d8206cc32e8cfd59744112895f63676
Joshua Mariano <joshuamariano.work@gmail.com>
2026-01-23 00:05:25 +0800

    Added X and Y label, modified plot

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
 # Simple plot
-plt.plot([1, 2, 3, 4, 8], [10, 20, 25, 30, 35])
-plt.title("Test Plot")
+plt.plot([2001, 2002, 2003, 2004, 2008], [10, 20, 25, 30, 35])
+plt.title("STD Cases in the Philippines") #for the title
+plt.xlabel("Year") #x-label title Year
+plt.ylabel("x1000")
 plt.show()
