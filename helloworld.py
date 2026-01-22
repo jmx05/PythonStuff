e4ab506b81ff135df3149b8855f1b4d0146d1dfd
Joshua Mariano <joshuamariano.work@gmail.com>
2026-01-23 00:08:19 +0800

    axis label comment

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 # Simple plot
 plt.plot([2001, 2002, 2003, 2004, 2008], [10, 20, 25, 30, 35])
 plt.title("STD Cases in the Philippines") #for the title
-plt.xlabel("Year") #x-label title Year
-plt.ylabel("x1000")
+plt.xlabel("Year") # X-axis title
+plt.ylabel("x1000") # Y-axis title
 plt.show()
