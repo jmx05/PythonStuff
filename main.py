bd284cef0018130c088479f79117e50d30866915
Joshua Mariano <joshuamariano.work@gmail.com>
2026-01-31 00:47:24 +0800

    modified user input capture

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-user_prompt = "Enter a task: " #you can save strings in variables and have them printed later
+#python list starts and ends with brackets, list is need to store multiple values in one variable
+#you can print lists directly
 
-todo1 = input(user_prompt) #input() allows the user to type something and you can directly store it in a variable
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
-
-todos = [todo1, todo2, todo3] #python list starts and ends with brackets, list is need to store multiple values in one variable
-print(todos) #you can print lists directly
-
-print(type(todos)) #type() tells you what type a variable is
+#type() tells you what type a variable is
 
 #numbers before variables aren't allowed
+#input() allows the user to type something and you can directly store it in a variable
+
+
+user_prompt = "Enter a task: " #you can save strings in variables and have them printed later
+
+while True: #under the while condition, everything indented will be repeated infinitely True is True
+    
+    todo = input(user_prompt) 
+    print("You have added the task:", todo)
